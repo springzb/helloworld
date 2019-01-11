c32c324100f1461f7b99e9cdca354461e1921c76
admin <admin@qq.com>
2019-01-11 21:27:26 +0800

    第一次提交

---
+++ src/main/java/cn/mesmile/HelloWorld.java
@@ -0,0 +1,14 @@
+package cn.mesmile;
+
+/**
+ * @Created with IDEA
+ * @author: Super Zheng
+ * @Description: java类作用描述
+ * @Date:2019/1/11
+ * @Time:20:42
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
